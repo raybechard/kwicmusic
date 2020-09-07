a48be7eaaa062a90abb4858fdc4797e41355ed4b
Ray Bechard <raybechard@gmail.com>
2020-09-07 10:24:06 -0400

    1st pass clean login

--- app/src/main/java/com/numerad/kwicmusic/ui/login/LoginActivity.kt
+++ app/src/main/java/com/numerad/kwicmusic/ui/login/LoginActivity.kt
@@ -4,14 +4,8 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ProgressBar
 import android.widget.Toast
-import androidx.annotation.StringRes
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.auth.api.signin.GoogleSignIn
@@ -22,16 +16,14 @@ import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.numerad.kwicmusic.R
 import com.numerad.kwicmusic.SessionManager
 import com.numerad.kwicmusic.auth.IdentityProvider
+import com.numerad.kwicmusic.auth.IdentityProvider.Companion.GOOGLE
 import com.numerad.kwicmusic.auth.TokenActivity
-import com.numerad.kwicmusic.databinding.ActivityLoginBinding
-import com.numerad.kwicmusic.domain.AuthenticationService
 import com.numerad.kwicmusic.ui.main.MainActivity
 import kotlinx.android.synthetic.main.activity_login.*
 import net.openid.appauth.*
@@ -42,45 +34,25 @@ import timber.log.Timber
 
 class LoginActivity : BaseActivity(), View.OnClickListener, KoinComponent {
 
-    lateinit var mAuthService: AuthorizationService
     private var googleSignInClient: GoogleSignInClient? = null
     private lateinit var loginViewModel: LoginViewModel
     private lateinit var auth: FirebaseAuth
-    lateinit var binding: ActivityLoginBinding
-    private val authenticationService: AuthenticationService by inject()
+    lateinit var authService: AuthorizationService
     private val sessionManager: SessionManager by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_login)
-
-        val username = findViewById<EditText>(R.id.username)
-        val password = findViewById<EditText>(R.id.password)
-        val login = findViewById<Button>(R.id.login)
-        val loading = findViewById<ProgressBar>(R.id.loading)
 
         loginViewModel = ViewModelProvider(this, LoginViewModelFactory())
             .get(LoginViewModel::class.java)
 
-        loginViewModel.loginFormState.observe(this@LoginActivity, Observer {
-            val loginState = it ?: return@Observer
-            login.isEnabled = loginState.isDataValid
-
-            if (loginState.usernameError != null) {
-                username.error = getString(loginState.usernameError)
-            }
-            if (loginState.passwordError != null) {
-                password.error = getString(loginState.passwordError)
-            }
-        })
-
-        loginViewModel.loginResult.observe(this@LoginActivity, Observer {
+        loginViewModel.loginResult.observe(this, Observer {
             val loginResult = it ?: return@Observer
 
             loading.visibility = View.GONE
             if (loginResult.error != null) {
-                showLoginFailed(loginResult.error)
+                Toast.makeText(applicationContext, loginResult.error, Toast.LENGTH_SHORT).show()
             }
             if (loginResult.success != null) {
                 updateUiWithUser(loginResult.success)
@@ -89,22 +61,16 @@ class LoginActivity : BaseActivity(), View.OnClickListener, KoinComponent {
             finish()
         })
 
+        sign_in_button.setOnClickListener(this)
+
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.google_client_id))
             .requestEmail()
             .build()
 
         googleSignInClient = GoogleSignIn.getClient(this, gso)
-
-        sign_in_button.setOnClickListener(this)
-
         auth = Firebase.auth
-
-        mAuthService = AuthorizationService(this)
-
-//        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
-//        binding.lifecycleOwner = this.viewLifecycleOwner
-//        binding.viewmodel = loginViewModel
+        authService = AuthorizationService(this)
     }
 
     override fun onStart() {
@@ -114,33 +80,24 @@ class LoginActivity : BaseActivity(), View.OnClickListener, KoinComponent {
         if (account != null)
             updateUiWithUser(account.toLoggedInUserView())
         else
-            Timber.d("User already signed-in")
+            Timber.d("Already signed-in")
     }
 
     private fun updateUiWithUser(model: LoggedInUserView) {
         val welcome = getString(R.string.welcome)
         val displayName = model.displayName
-
-//        val intent = Intent(this, MainActivity::class.java)
-//        startActivity(intent)
-//        finish()
-
         Toast.makeText(applicationContext, "$welcome $displayName", Toast.LENGTH_LONG).show()
-    }
-
-    private fun showLoginFailed(@StringRes errorString: Int) {
-        Toast.makeText(applicationContext, errorString, Toast.LENGTH_SHORT).show()
+        gotoMainActivity()
     }
 
     override fun onClick(v: View) {
         when (v.id) {
-            R.id.sign_in_button -> signIn()
+            R.id.sign_in_button -> {
+//                loginViewModel.login(username.text.toString(), password.text.toString())
+                val signInIntent: Intent = googleSignInClient?.signInIntent ?: return
+                startActivityForResult(signInIntent, RC_SIGN_IN)
+            }
         }
-    }
-
-    private fun signIn() {
-        val signInIntent: Intent = googleSignInClient?.signInIntent ?: return
-        startActivityForResult(signInIntent, RC_SIGN_IN)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -151,17 +108,27 @@ class LoginActivity : BaseActivity(), View.OnClickListener, KoinComponent {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
             try {
                 // Google Sign In was successful, authenticate with Firebase
-                val account = task.getResult(ApiException::class.java)!!
-                Timber.tag(TAG).d("firebaseAuthWithGoogle:%s", account.id)
+                val account = task.getResult(ApiException::class.java)
+
+                if (account == null) {
+                    Timber.tag(TAG).e("account == null")
+                    Snackbar.make(container, "account == null", Snackbar.LENGTH_SHORT).show()
+                    return
+                }
 
                 firebaseAuthWithGoogle(account.idToken!!)
 //                handleSignInResult(task)
             } catch (e: ApiException) {
-                // Google Sign In failed, update UI appropriately
                 Timber.tag(TAG).e(e, e.localizedMessage)
-                // ...
+                Snackbar.make(container, "Google Sign In failed", Snackbar.LENGTH_SHORT).show()
             }
         }
+    }
+
+    private fun gotoMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 
     private fun firebaseAuthWithGoogle(idToken: String) {
@@ -171,30 +138,13 @@ class LoginActivity : BaseActivity(), View.OnClickListener, KoinComponent {
         auth.signInWithCredential(credential)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Timber.d("signInWithCredential:success")
                     val user = auth.currentUser
-//                    updateUI(user)
-
-                    // todo ray how to get oauth2 access token here?
-//                    val vv = authenticationService.getAuth(
-//                        getString(R.string.server_client_id),
-//                        "code",
-//                        "openid email",
-//                        "uri",
-//                        generateNonce(15))
-//                    getConfig()
+                    getConfig()
 //                    sessionManager.saveAuthToken(token)
-
-                    val intent = Intent(this, MainActivity::class.java)
-                    startActivity(intent)
-                    finish()
+                    gotoMainActivity()
                 } else {
-                    // If sign in fails, display a message to the user.
-                    Timber.tag(TAG).w(task.exception, "signInWithCredential:failure")
-//                    val view = binding.container
-                    Snackbar.make(container, "Authentication Failed.", Snackbar.LENGTH_SHORT).show()
-//                    updateUI(null)
+                    Timber.tag(TAG).e(task.exception, "FirebaseAuth.signInWithCredential failure")
+                    Snackbar.make(container, "Authentication Failed", Snackbar.LENGTH_SHORT).show()
                 }
 
                 hideProgressBar()
@@ -204,82 +154,27 @@ class LoginActivity : BaseActivity(), View.OnClickListener, KoinComponent {
     private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
         try {
             val account = completedTask.getResult(ApiException::class.java)
-
-            // Signed in successfully, show authenticated UI.
             if (account != null)
                 updateUiWithUser(account.toLoggedInUserView())
             else
                 Timber.e("Can't sign in")
 
         } catch (e: ApiException) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Timber.tag(TAG).w("signInResult:failed code=%s", e.statusCode)
-//            updateUiWithUser(null)
+            Timber.tag(TAG).e("signInResult:failed code=%s", e.statusCode)
+            Snackbar.make(container, "Sign in Failed", Snackbar.LENGTH_SHORT).show()
         }
-    }
-
-    private fun updateUI(user: FirebaseUser?) {
-        hideProgressBar()
-        if (user != null) {
-//            binding.status.text = getString(R.string.google_status_fmt, user.email)
-//            binding.detail.text = getString(R.string.firebase_status_fmt, user.uid)
-
-            binding.signInButton.visibility = View.GONE
-//            binding.signOutAndDisconnect.visibility = View.VISIBLE
-        } else {
-//            binding.status.setText(R.string.signed_out)
-//            binding.detail.text = null
-
-            binding.signInButton.visibility = View.VISIBLE
-//            binding.signOutAndDisconnect.visibility = View.GONE
-        }
-    }
-
-    companion object {
-        private const val TAG = "LoginActivity"
-        private const val RC_SIGN_IN = 9001
-    }
-
-    fun generateNonce(length: Int): String {
-        val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
-        return (1..length)
-            .map { allowedChars.random() }
-            .joinToString("")
     }
 
     private fun getConfig() {
-        val providers: List<IdentityProvider> =
-            IdentityProvider.getEnabledProviders(this)
-
-        for (idp in providers) {
-            val retrieveCallback =
-                RetrieveConfigurationCallback { serviceConfiguration: AuthorizationServiceConfiguration?, ex: AuthorizationException? ->
-                    if (ex != null) {
-                        Timber.tag(TAG).w(ex, ex.localizedMessage)
-                    } else {
-                        Timber.tag(TAG).d("configuration retrieved for %s", idp.name)
-
-                        if (serviceConfiguration != null)
-                            makeAuthRequest(serviceConfiguration, idp)
-                        else
-                            Timber.tag(TAG).e("serviceConfiguration is null")
-                    }
+        GOOGLE.retrieveConfig(
+            this,
+            RetrieveConfigurationCallback { config: AuthorizationServiceConfiguration?, ex: AuthorizationException? ->
+                when {
+                    ex != null -> Timber.tag(TAG).w(ex, ex.localizedMessage)
+                    config == null -> Timber.tag(TAG).e("AuthorizationServiceConfiguration is null")
+                    else -> makeAuthRequest(config, GOOGLE)
                 }
-
-//            val idpButton = Button(this, null, R.style.Widget_AppCompat_Button_Borderless)
-//            idpButton.setBackgroundResource(idp.buttonImageRes)
-//            idpButton.contentDescription = resources.getString(idp.buttonContentDescriptionRes)
-//            idpButton.width = resources.getDimensionPixelSize(R.dimen.idp_button_edge_size)
-//            idpButton.height = resources.getDimensionPixelSize(R.dimen.idp_button_edge_size)
-//            idpButton.setOnClickListener {
-//                Log.d(MainActivity.TAG, "initiating auth for " + idp.name)
-//                idp.retrieveConfig(this@MainActivity, retrieveCallback)
-//            }
-//            idpButtonContainer.addView(idpButton)
-
-            idp.retrieveConfig(this, retrieveCallback)
-        }
+            })
     }
 
     private fun makeAuthRequest(
@@ -297,7 +192,7 @@ class LoginActivity : BaseActivity(), View.OnClickListener, KoinComponent {
 
         Timber.tag(TAG).d("Making auth request to %s", idp.name)
 
-        mAuthService.performAuthorizationRequest(
+        authService.performAuthorizationRequest(
             authRequest,
             TokenActivity.createPostAuthorizationIntent(
                 this,
@@ -305,25 +200,18 @@ class LoginActivity : BaseActivity(), View.OnClickListener, KoinComponent {
                 serviceConfig.discoveryDoc,
                 idp.clientSecret
             ),
-            mAuthService.createCustomTabsIntentBuilder()
+            authService.createCustomTabsIntentBuilder()
 //                .setToolbarColor(getCustomTabColor())
                 .build()
         )
     }
-}
 
-fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
-    this.addTextChangedListener(object : TextWatcher {
-        override fun afterTextChanged(editable: Editable?) {
-            afterTextChanged.invoke(editable.toString())
-        }
+    private fun GoogleSignInAccount.toLoggedInUserView() = LoggedInUserView("unknown").also {
+        it.displayName = displayName ?: "unknown"
+    }
 
-        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-
-        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
-    })
-}
-
-fun GoogleSignInAccount.toLoggedInUserView() = LoggedInUserView("unknown").also {
-    it.displayName = displayName ?: "unknown"
+    companion object {
+        private const val TAG = "LoginActivity"
+        private const val RC_SIGN_IN = 9001
+    }
 }
